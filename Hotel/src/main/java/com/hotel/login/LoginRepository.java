6d8be035f5d72ae27dcf9c768711029acab9a343
Aileen Alvarado <monsealvarado775@gmail.com>
2023-09-13 16:55:24 -0600

    redireccionando login

--- Hotel/src/main/java/com/hotel/login/LoginRepository.java
+++ Hotel/src/main/java/com/hotel/login/LoginRepository.java
@@ -1,10 +1,12 @@
 package com.hotel.login;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hotel.huesped.HuespedEntity;
 
 public interface LoginRepository extends JpaRepository<HuespedEntity, Long>{
 
-	 public HuespedEntity findByNameAndPassword(String name, String password);
+	 public Optional<HuespedEntity> findByNameAndPassword(String name, String password);
 }
